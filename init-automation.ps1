5a5135a78d86514da5ae5fff304179b685423681
James Ambrose <galaxy2198@gmail.com>
2020-06-15 12:46:01 -0500

    added plex and organizr

--- init-automation.ps1
+++ init-automation.ps1
@@ -12,68 +12,148 @@ $sonarCfg = "sonarr-docker-compose-config.yml"
 $radarrCfg = "radarr-docker-compose-config.yml"
 $jackettCfg = "jackett-docker-compose-config.yml"
 $sabnzbdCfg = "sabnzbd-docker-compose-config.yml"
+$plexCfg = "plex-docker-compose.config.yml"
+$organizrCfg = "organizr-docker-compose-config.yml"
 $dockerComposeURL = "https://docs.docker.com/compose/"
 
-##### Sonarr #####
-
-Write-Host "Attempting to start Sonarr container"
-docker-compose -f $sonarCfg up -d --remove-orphans
-
-if (!$?)
+if ($null -eq $args[0])
 {
-     Write-Host "Failed to start Sonarr is docker running with docker-compose installed? $dockerComposeURL"
-     Write-Host "If so please check that $sonarCfg is up to date."
+     Write-Warning "Unexpected input! Exptected parameters are: `"start`" & `"cleanup`"" 
      exit
 }
-elseif ($?)
-{
-     Write-Host "Sonarr was successfully brought online. Live at http://$(hostname):8989"
+
+if ($args[0].ToLower() -eq "start"){
+     ##### Sonarr #####
+
+     Write-Host "Attempting to start Sonarr container"
+     docker-compose -f $sonarCfg up -d
+
+     if (!$?)
+     {
+          Write-Host "Failed to start Sonarr is docker running with docker-compose installed? $dockerComposeURL"
+          Write-Host "If so please check that $sonarCfg is up to date."
+          exit
+     }
+     elseif ($?)
+     {
+          Write-Host "Sonarr was successfully brought online. Live at http://$(hostname):8989"
+     }
+
+     ##### Radarr #####
+
+     Write-Host "Attempting to start Radarr container"
+     docker-compose -f $radarrCfg up -d
+
+     if (!$?)
+     {
+          Write-Host "Failed to start Sonarr is docker running with docker-compose installed? $dockerComposeURL"
+          Write-Host "If so please check that $radarrCfg is up to date."
+          exit
+     }
+     elseif ($?)
+     {
+          Write-Host "Sonarr was successfully brought online. Live at http://$(hostname):7878"
+     }
+
+     ##### Jackett #####
+
+     Write-Host "Attempting to start Jackett container"
+     docker-compose -f $jackettCfg up -d
+
+     if (!$?)
+     {
+          Write-Host "Failed to start Sonarr is docker running with docker-compose installed? $dockerComposeURL"
+          Write-Host "If so please check that $jackettCfg is up to date."
+          exit
+     }
+     elseif ($?)
+     {
+          Write-Host "Sonarr was successfully brought online. Live at http://$(hostname):9117"
+     }
+
+     ##### sabnzbd #####
+
+     Write-Host "Attempting to start sabnzbd container"
+     docker-compose -f $sabnzbdCfg up -d
+
+     if (!$?)
+     {
+          Write-Host "Failed to start Sonarr is docker running with docker-compose installed? $dockerComposeURL"
+          Write-Host "If so please check that $sabnzbdCfg is up to date."
+          exit
+     }
+     elseif ($?)
+     {
+          Write-Host "Sonarr was successfully brought online. Live at http://localhost:8080"
+     }
+
+     ##### plex #####
+
+     Write-Host "Attempting to start Plex container"
+     docker-compose -f $plexCfg up -d
+
+     if (!$?)
+     {
+          Write-Host "Failed to start Sonarr is docker running with docker-compose installed? $dockerComposeURL"
+          Write-Host "If so please check that $plexCfg is up to date."
+          exit
+     }
+     elseif ($?)
+     {
+          Write-Host "Sonarr was successfully brought online. Live at http://$(hostname):32400"
+     }
+
+     ##### Organizr #####
+
+     Write-Host "Attempting to start Organizr container"
+     docker-compose -f $organizrCfg up -d
+
+     if (!$?)
+     {
+          Write-Host "Failed to start Sonarr is docker running with docker-compose installed? $dockerComposeURL"
+          Write-Host "If so please check that $organizrCfg is up to date."
+          exit
+     }
+     elseif ($?)
+     {
+          Write-Host "Sonarr was successfully brought online. Live at http://$(hostname):9983"
+     }
 }
-
-##### Radarr #####
-
-Write-Host "Attempting to start Radarr container"
-docker-compose -f $radarrCfg up -d --remove-orphans
-
-if (!$?)
+elseif ($args[0].ToLower() -eq "cleanup")
 {
-     Write-Host "Failed to start Sonarr is docker running with docker-compose installed? $dockerComposeURL"
-     Write-Host "If so please check that $radarrCfg is up to date."
-     exit
+     docker-compose -f $sonarCfg down
+     docker-compose -f $radarrCfg down
+     docker-compose -f $jackettCfg down
+     docker-compose -f $sabnzbdCfg down
+     docker-compose -f $plexCfg down
+     docker-compose -f $organizrCfg down
+
+     if (Test-Path -Path jackket/)
+     {
+          Remove-Item -Recurse jackket/
+     }
+     if (Test-Path -Path sonarr/)
+     {
+          Remove-Item -Recurse sonarr/
+     }
+     if (Test-Path -Path radarr/)
+     {
+          Remove-Item -Recurse radarr/
+     }
+     if (Test-Path -Path sabnzbd/)
+     {
+          Remove-Item -Recurse sabnzbd/
+     }
+     if (Test-Path -Path plex/)
+     {
+          Remove-Item -Recurse plex/
+     }
+     if (Test-Path -Path organizr/)
+     {
+          Remove-Item -Recurse organizr/
+     }
 }
-elseif ($?)
+else 
 {
-     Write-Host "Sonarr was successfully brought online. Live at http://$(hostname):7878"
-}
-
-##### Jackett #####
-
-Write-Host "Attempting to start Jackett container"
-docker-compose -f $jackettCfg up -d --remove-orphans
-
-if (!$?)
-{
-     Write-Host "Failed to start Sonarr is docker running with docker-compose installed? $dockerComposeURL"
-     Write-Host "If so please check that $jackettCfg is up to date."
-     exit
-}
-elseif ($?)
-{
-     Write-Host "Sonarr was successfully brought online. Live at http://$(hostname):9117"
-}
-
-##### sabnzbd #####
-
-Write-Host "Attempting to start sabnzbd container"
-docker-compose -f $sabnzbdCfg up -d --remove-orphans
-
-if (!$?)
-{
-     Write-Host "Failed to start Sonarr is docker running with docker-compose installed? $dockerComposeURL"
-     Write-Host "If so please check that $sabnzbdCfg is up to date."
-     exit
-}
-elseif ($?)
-{
-     Write-Host "Sonarr was successfully brought online. Live at http://$(hostname):8080"
+     Write-Host "Unexpected input! Exptected parameters are: `"start`" & `"cleanup`""     
 }
